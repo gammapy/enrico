2cf5211e844c88efbe148ac4d59f4a26814c5420
Christoph Deil <Deil.Christoph@gmail.com>
2011-12-09 13:14:32 +0100

    Minor changes. No good solution for init found yet.

--- init.sh
+++ init.sh
@@ -1,13 +1,25 @@
-echo "Setting up Fermi and Enrico on the MPIK cluster"
+# Example enrico init script for the MPIK cluster
+# TODO: Until we find a better solution (e.g. ask user interactively?)
+# you have to manually adapt it to your system.
+# Maybe the best is to only work with some environment variables
+# FERMI_DIR, FERMI_DATA_DIR and enrico is installed, i.e. no ENRICO_DIR?
 
-export FERMI_DIR=/lfs/l1/hess/users/deil/bin/HESSSurveyBin/fermi/latest/x86_64-unknown-linux-gnu-libc2.5
-export FERMI=/home/hfm/deil/storage/fermi/public
-export FERMI_CATALOG_DIR=$FERMI/catalog
-export FERMI_DIFFUSE_DIR=$FERMI/diffuse
-export FERMI_DOWNLOAD_DIR=$FERMI/download
-export FERMI_DATA_DIR=$FERMI/data
+# This should be done as an extra step I think
+#echo "Initializing Fermi Science Tools"
+#export FERMI_DIR=/lfs/l1/hess/users/deil/bin/HESSSurveyBin/fermi/latest/x86_64-unknown-linux-gnu-libc2.5
+#source $FERMI_DIR/fermi-init.sh
 
-source $FERMI_DIR/fermi-init.sh
+echo "Adding Enrico to PATH and PYTHONPATH"
+export ENRICO_DIR=/home/hfm/deil/git/enrico
+export PATH=$PATH:$ENRICO_DIR/scripts
+export PYTHONPATH=$ENRICO_DIR:$PYTHONPATH
 
-export PATH=$PATH:/nfs/d22/hfm/sanchez/enrico/scripts
-export PYTHONPATH=$PYTHONPATH:/nfs/d22/hfm/sanchez/enrico
+echo "Setting environment variables for data file locations"
+export FERMI_DATA_DIR=/home/hfm/deil/storage/fermi/public
+export FERMI_CATALOG_DIR=$FERMI_DATA_DIR/catalog
+export FERMI_DIFFUSE_DIR=$FERMI_DATA_DIR/diffuse
+export FERMI_DOWNLOAD_DIR=$FERMI_DATA_DIR/download
+export FERMI_PREPROCESSED_DIR=$FERMI/preprocessed
+
+echo "Run enrico_setup to check if you are set up correctly."
+echo "Check your PATH and PYTHONPATH if enrico_setup is not found or gives an error."
