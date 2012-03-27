a72e8b6d6631902d1687b4504e45d383bd1c88e7
David Sanchez <david.andre.sanchez@gmail.com>
2012-03-27 11:01:55 +0200

    improve the TS map

--- enrico/tsmap.py
+++ enrico/tsmap.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os
+import os,sys
 import pyfits
 import SummedLikelihood
 import utils
@@ -8,10 +8,151 @@ from config import get_config
 import environ
 from gtfunction import Observation
 
-def _PixelFile(folder,i,j):
-    return folder+'/TSMap/Pixel_'+str(i)+'_'+str(j)
+class TSMap:
+    # This class groups all the need functions and 
+    # variables to compute a TS map
+    # Variables : RAref, DECref and binsz are related to the cmap used
+    # to define the grip for the TS map and are respectively 
+    # the center of the map and the size of 1 bin in degrees
+    # tsfolder is the location where the produced files file be stored
+    # npix is the number of pixel of the TS map
+    # infile is the used configuration file
+
+    # A TS map can be computed by submiting jobs to a cluster
+    # There is two way to compute a TS map : Each job
+    # computes the TS in one pixel or in one row
+    def __init__(self,infile):
+        self.config = get_config(infile)
+        self.tsfolder = self.config['out']+"/TSMap"
+        self.TSfits = self.config['target']['name']+'_'+self.config['file']['tag']+"_TSMap.fits"
+        self.infile = infile
+        self.npix = self.config['TSMap']['npix']
+        self.RAref = 0
+        self.DECref = 0
+        self.binsz = 0
+
+    def _launch(self,ra,dec,i,j):
+        """ Launch a job (either pixel evaluation or row evaluation). 
+        Can be the submittion of a job to a cluster """
+        enricodir = environ.DIRS.get('ENRICO_DIR')
+        cmd = enricodir+"/enrico/"+sys.argv[0]+" "+os.getcwd()+"/"+self.infile +" "+ str(ra) +" "+ str(dec) +" "+ str(i) +" "+ str(j) #cmd line to send
+
+        if self.config['TSMap']['Submit'] == 'yes':
+            prefix = self.tsfolder + "/TSMap_" + str(i) +"_"+ str(j)
+            scriptname = prefix + "_Script.sh"
+            JobLog = prefix + "_Job.log"
+            JobName = (self.config['target']['name'] + "_TSMap_" + str(i) +"_"+ str(j))
+            call(cmd, enricodir, scriptname, JobLog, JobName) #submition
+        else : 
+            os.system(cmd) #run directly 
+
+    def _PixelFile(self,i,j):
+        """ return the name of a file where the result of 1 pixel 
+        evaluation will be stored"""
+        return self.tsfolder+'/Pixel_'+str(i)+'_'+str(j)
+
+    def FitOnePixel(self,ra,dec,i,j) :
+        """Run a evaluation of the pixel (i,j) corresponding to position (ra,dec)"""
+        outXml = utils._dump_xml(self.config)
+        folder = self.config['out']
+
+        Fit = GetFitObjectForTSmap(self.config,xmlfile=outXml) #get the Fit object
+
+        src = GetSrc(Fit,ra,dec) # get the Source object at postion ra dec
+
+        if self.config['TSMap']['RemoveTarget'] : # remove the target is asked
+            Fit.deleteSource(self.config['target']['name'])
+
+        if self.config['TSMap']['Re-Fit']: # reoptimze before is asked
+            Fit.fit(0,optimizer=self.config['fitting']['optimizer'])
+
+        for par in xrange(Fit.logLike.getNumParams()): # freeze all the source parameters
+            Fit[par].setFree(0)
+
+        Fit.addSource(src)# add a spurious source
+        # dump the *new* xml file
+        Fit.writeXml(self.tsfolder+"/model_"+str(ra)+"_"+str(dec)+".xml") 
+
+        # a new Fit object with the new xml file is needed.
+        # just changing the position of the spurious source does not work 
+        TSFit = GetFitObjectForTSmap(self.config,xmlfile=self.tsfolder+"/model_"+str(ra)+"_"+str(dec)+".xml")
+
+        TSFit.fit(0,optimizer=self.config['fitting']['optimizer'])
+
+        # save the result
+        fsave = open(self._PixelFile(i,j),'w')
+        fsave.write(str(ra)+"\t"+str(dec)+"\t"+
+                    str(TSFit.Ts("Spurious"))+"\t"+
+                    str(TSFit.logLike.value()))
+        fsave.close()
+
+    def FitOneRow(self,ra,i) :
+        """ function which run the evaluation of 1 row of the TS map
+        using a loop and calling the fit for 1 pixel"""
+        for j in xrange(self.npix):
+            dec = self.DECref + binsz*(j-npix/2.)
+            self.FitOnePixel(ra,dec,i,j)
+
+    def runTSMap(self) :
+        """ Run a TS map using the configuration file given"""
+        folder = self.config['out']
+        os.system('mkdir -p ' + self.tsfolder)
+
+        # Read the cmap produced before to get the grid for the TS map
+        runfit = Observation(folder, self.config)
+        cmap = pyfits.open(runfit.cmapfile)
+        npix_im = min(cmap[0].header['NAXIS1'],cmap[0].header['NAXIS2'])
+        self.npix = min(self.npix,npix_im)
+        self.RAref = cmap[0].header['CRVAL1']
+        self.DECref = cmap[0].header['CRVAL2']
+        self.binsz = cmap[0].header['CDELT1']
+
+        for i in xrange(self.npix): #loop over the X axis
+            ra = self.RAref + self.binsz*(i-self.npix/2.)
+            if self.config['TSMap']['method'] == 'row' : # a row is evaluated in once
+                     print 'Run Row evaluation at ',ra
+                     self._launch(ra,0,i,0)
+            else : # each pixel is evaluated by one job
+                for j in xrange(self.npix): #loop over the Y axis
+                     dec = self.DECref + self.binsz*(j-self.npix/2.)
+                     print 'Run Pixel evaluation at ',ra,' ',dec
+                     self._launch(ra,dec,i,j) 
+
+    def PlotTSmap(self) :
+        """ Gather the results of the evaluation of 
+        each pixel and fill a fits file"""
+        folder = self.config['out']
+
+        # Read the cmap produced before to get the grid for the TS map
+        runfit = Observation(folder, self.config)
+        header = pyfits.getheader(runfit.cmapfile)
+        data = pyfits.getdata(runfit.cmapfile)*0
+        npix_im = min(header['NAXIS1'],header['NAXIS2'])
+        npix = min(self.config['TSMap']['npix'],npix_im)
+        Xref = header['CRPIX1']
+        Yref = header['CRPIX2']
+        binsz = header['CDELT1']
+
+        print npix
+        import string # read the results
+        for i in xrange(npix):
+            for j in xrange(npix):
+                try : 
+                    lines = open(self._PixelFile(i,j),"r").readlines()
+                    Value = float(string.split(lines[0])[2])
+                except :
+                    print "Cannot find, open or read ",self._PixelFile(i,j)
+                    Value = 0.
+                data[Xref+ (i-npix/2.)][Yref+ (j-npix/2.)] = Value
+
+        # save in a fits files
+        pyfits.writeto(folder+"/"+self.TSfits,data,header)
+        print "TS Map saved in "+folder+"/"+self.TSfits
+
 
 def GetSrc(Fit,ra,dec):
+    """ return a Source object by cloning a pointlike source from the Fit object
+    the source is rename, move to (ra,dec) and the spetral model is change to PowerLaw"""
     ind = 0
     for name in  Fit.logLike.srcNames() :
         src = Fit.logLike.getSource(name).clone()
@@ -32,133 +173,44 @@ def GetSrc(Fit,ra,dec):
 
             return src
 
-
-def FitOnePixel(infile,ra,dec,i,j) :
-    """@todo: document me"""
-    config = get_config(infile)
-    outXml = utils._dump_xml(config)
-
-    folder = config['out']
-
+def GetFitObjectForTSmap(config,xmlfile=""):
+    """ return a fit object with the xmlmodel provided."""
     if config['Spectrum']['SummedLike'] == 'yes':
         # Create two obs instances
         runfitfront, _ = utils.Analysis(folder, config, tag="FRONT", convtyp=0)
         runfitback, _ = utils.Analysis(folder, config, tag="BACK", convtyp=1)
-        runfitfront.obs.xmlfile = outXml
-        runfitback.obs.xmlfile = outXml
+        if not(outXml ==""):
+            runfitfront.obs.xmlfile = outXml
+            runfitback.obs.xmlfile = outXml
         FitB = runfitback.CreateFit()
         FitF = runfitfront.CreateFit()
         Fit = SummedLikelihood.SummedLikelihood()
         Fit.addComponent(FitB)
         Fit.addComponent(FitF)
-        runfit = runfitback
 
     else:
         convtype = config['analysis']['convtype']
         # Create one obs instanceFit.addSource
         runfit, _ = utils.Analysis(folder, config, tag="", convtyp=convtype)
-        runfit.obs.xmlfile = outXml
-
+        if not(outXml ==""):
+            runfit.obs.xmlfile = outXml
         Fit = runfit.CreateFit()
-
-    src = GetSrc(Fit,ra,dec)
-
-    if config['TSMap']['RemoveTarget'] :
-        Fit.deleteSource(config['target']['name'])
-
-    if config['TSMap']['Re-Fit']:
-       Fit.fit(0,optimizer=config['fitting']['optimizer'])
-
-    for par in xrange(Fit.logLike.getNumParams()):
-        Fit[par].setFree(0)
-
-    Fit.addSource(src)
-
-    Fit.writeXml(folder+"/TSMap/model_"+str(ra)+"_"+str(dec)+".xml")
-    runfit.obs.xmlfile = folder+"/TSMap/model_"+str(ra)+"_"+str(dec)+".xml"
-
-    TSFit = runfit.CreateFit()
-    TSFit.fit(0,optimizer=config['fitting']['optimizer'])
-    TS = TSFit.Ts("Spurious")
-    loglike = TSFit.logLike.value()
-
-    fsave = open(_PixelFile(folder,i,j),'w')
-    fsave.write(str(ra)+"\t"+str(dec)+"\t"+str(TS)+"\t"+str(loglike))
-    fsave.close()
-
-def runTSMap(infile) :
-    config = get_config(infile)
-    
-    folder = config['out']
-    os.system('mkdir -p ' + folder+'/TSMap')
-    runfit = Observation(folder, config)
-
-    cmap = pyfits.open(runfit.cmapfile)
-
-    npix_im = min(cmap[0].header['NAXIS1'],cmap[0].header['NAXIS2'])
-    npix = min(config['TSMap']['npix'],npix_im)
-    RAref = cmap[0].header['CRVAL1']
-    DECref = cmap[0].header['CRVAL2']
-
-    binsz = cmap[0].header['CDELT1']
-
-    enricodir = environ.DIRS.get('ENRICO_DIR')
-    prog = enricodir+"/enrico/tsmap.py "+os.getcwd()+"/"+infile
-
-    for i in xrange(npix):
-        for j in xrange(npix):
-             ra = RAref + binsz*(i-npix/2.)
-             dec = DECref + binsz*(j-npix/2.)
-             print 'Run Pixel evaluation at ',ra,' ',dec
-
-             cmd = prog +" "+ str(ra) +" "+ str(dec) +" "+ str(i) +" "+ str(j)
-             prefix = config['out'] + "/TSMap/TSMap_" + str(i) +"_"+ str(j)
-             scriptname = prefix + "_Script.sh"
-             JobLog = prefix + "_Job.log"
-             JobName = (config['target']['name'] + "_TSMap_" + str(i) +"_"+ str(j))
-             call(cmd, enricodir, scriptname, JobLog, JobName)
-
-
-def PlotTSmap(infile) :
-    config = get_config(infile)
-    
-    folder = config['out']
-    runfit = Observation(folder, config)
-
-    header = pyfits.getheader(runfit.cmapfile)
-    data = pyfits.getdata(runfit.cmapfile)*0
-
-    npix_im = min(header['NAXIS1'],header['NAXIS2'])
-    npix = min(config['TSMap']['npix'],npix_im)
-
-    Xref = header['CRPIX1']
-    Yref = header['CRPIX2']
-
-    binsz = header['CDELT1']
-
-    import string
-    for i in xrange(npix):
-        for j in xrange(npix):
-            try : 
-                lines = open(_PixelFile(folder,i,j),"r").readlines()
-                Value = float(string.split(lines[0])[2])
-            except :
-                print "Cannot find, open or read ",_PixelFile(folder,i,j)
-                Value = 0.
-            data[Xref+ (i-npix/2.)][Yref+ (j-npix/2.)] = Value
-
-    pyfits.writeto(folder+"/TSMap.fits",data,header)
-    print "TS Map saved in "+folder+"/TSMap.fits"
+    return Fit
 
 if __name__ == '__main__':
-    import sys
     try:
         infile = sys.argv[1]
     except:
         print('FATAL: Config file not found.')
         sys.exit(1)
 
-    if len(sys.argv)==2 :
-         print "TO BE IMPLEMENTED"
+    TSmap = TSMap(infile)
+
+    if len(sys.argv)== 6 :
+        if TSmap.config['TSMap']['method'] == 'row' :
+            TSmap.FitOneRow(float(sys.argv[2]),int(sys.argv[4]))
+        else :
+            TSmap.FitOnePixel(float(sys.argv[2]),float(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]))
     else :
-        FitOnePixel(infile,float(sys.argv[2]),float(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]))
+        print "Wrong number of arguments"
+        sys.exit(1)
