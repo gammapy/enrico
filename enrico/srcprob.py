4c004b3dcbcfc3a41e9efc15a2c39cb85f02e6fa
davidsanchez <david.andre.sanchez@gmail.com>
2014-03-18 16:48:05 +0100

    small changes in srcprob.py

--- enrico/srcprob.py
+++ enrico/srcprob.py
@@ -6,6 +6,13 @@ from enrico.utils import calcAngSepDeg
 from enrico.config import get_config
 import pyfits
 
+def Print(indices):
+      print "Energy\tAngular Sep\tProba"
+      for i in xrange(min(10,indices.size)):
+          angSep = calcAngSepDeg(config['target']["ra"],config['target']["dec"],ra[indices[i]],dec[indices[i]])
+          print "%2.1f\t%2.3f\t\t%2.5f"%(energy[indices[i]],angSep,proba[indices[i]])
+
+
 def Runsrcprob(config):
     config['space']['rad'] = config['srcprob']['rad']
     Obs = Observation(config['out'], config, config['analysis']['convtype'], tag="srcprob")
@@ -21,10 +28,11 @@ def Runsrcprob(config):
       ra = probfile[1].data.field("RA")
       dec = probfile[1].data.field("DEC")
       indices = energy.argsort()
-      print "Energy\tAngular Sep\tProba"
-      for i in xrange(min(10,indices.size)):
-          angSep = calcAngSepDeg(config['target']["ra"],config['target']["dec"],ra[indices[i]],dec[indices[i]])
-          print "%2.1f\t%2.3f\t\t%2.5f"%(energy[indices[i]],angSep,proba[indices[i]])
-
+      print "Results sorted by decreasing energy"
+      Print(indices)
+      print 
+      print "Results sorted by decreasing probability"
+      indices = proba.argsort()
+      Print(indices)
 
 
