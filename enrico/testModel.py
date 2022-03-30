41968d477c727680a7964899dd3972c2b273fd2a
david <david.sanchez@lapp.in2p3.fr>
2022-03-30 13:18:30 +0200

    fix testModel

--- enrico/testModel.py
+++ enrico/testModel.py
@@ -7,6 +7,7 @@ except:
     print("cPickle not found, using pickle instead")
 from enrico.fitmaker import FitMaker
 from enrico.gtfunction import Observation
+from enrico.RunGTlike import GenAnalysisObjects
 from enrico import Loggin
 from enrico import utils
 
@@ -21,60 +22,25 @@ class ModelTester(Loggin.Message):
         utils.mkdir_p(self.folder+"/TestModel")
         self.modellist = ["PowerLaw","LogParabola","PLSuperExpCutoff"]
         
-        '''
-        try:
-            with open(self.folder+"/TestModel/Fit.pickle","r") as pfile:
-                print("Retrieving previous Fit from %s" \
-                    %(self.folder+"/TestModel/Fit.pickle"))
-                self.FitRunner = pickle.load(pfile)
-                self.Fit = self.FitRunner.CreateLikeObject()
-        except:
-            self._GenFit()
-            self.FitRunner.PerformFit(self.Fit, False)
 
-            with open(self.folder+"/TestModel/Fit.pickle","w") as pfile:
-                print("Saving current Fit to %s" \
-                    %(self.folder+"/TestModel/Fit.pickle"))
-                pickle.dump(self.FitRunner,pfile)
-        '''
-        self._GenFit()
+        self.FitRunner,self.Fit,self.ListOfAnalysisObjects = GenAnalysisObjects(config)
         self.FitRunner.PerformFit(self.Fit, False)
 
         # Store the results in a dictionnary
         self.Results = {}
 
-    def _GenFit(self):
-         try :
-             del self.Fit
-         except :
-             pass
-
-         if self.config['Spectrum']['SummedLike'] == 'yes':
-             Obs1 = Observation(self.folder, self.config, tag="FRONT")
-             Obs2 = Observation(self.folder, self.config, tag="BACK")
-             self.FitRunnerfront = FitMaker(Obs1, self.config)
-             self.FitRunnerback = FitMaker(Obs2, self.config)
-             self.FitRunnerfront.CreateLikeObject()
-             self.FitRunnerback.CreateLikeObject()
-             self.Fit = SummedLikelihood.SummedLikelihood()
-         else:
-             Obs = Observation(self.folder, self.config, tag="")
-             self.FitRunner = FitMaker(Obs, self.config)##Class
-             #self.Fit = self.FitRunner.CreateLikeObject()
-             self.Fit = SummedLikelihood.SummedLikelihood()
-             self.Fit.addComponent(self.FitRunner.CreateLikeObject())
 
     def _printResults(self):
         print() 
         self.info("Summary of the results")
         for key in self.modellist:
            if key == "PowerLaw":
-               print((key," Log(Like) = ",self.Results[key]))
+               print(key," Log(Like) = ",self.Results[key])
                llpl = self.Results[key]
            else :
                TS = 2*(self.Results[key]-llpl)
                prob = 1 - scipy.stats.chi2.cdf(TS, 1)
-               print((key," Log(Like) = ",self.Results[key]," TS = ",TS," Pvalue = ",prob))
+               print(key," Log(Like) = ",self.Results[key]," TS = ",TS," Pvalue = ",prob)
 
     def TestModel(self):
         """ actually test the models """
@@ -100,8 +66,8 @@ class ModelTester(Loggin.Message):
             pars = content[1:]
 
             if model not in self.modellist:
-                print(("WARNING: given model %s not in the valid range %s" \
-                    %(str(model), str(self.modellist))))
+                print("WARNING: given model %s not in the valid range %s" \
+                    %(str(model), str(self.modellist)))
                 continue
 
             for k in range(len(pars)):
@@ -111,13 +77,13 @@ class ModelTester(Loggin.Message):
             # Reduce the list of possible models to the current one
             self.modellist = [model]
 
-            print(("Using model %s with parameters %s" %(str(model), str(pars))))
+            print("Using model %s with parameters %s" %(str(model), str(pars)))
             self.Results[model] = self.RunAFit(self.config["target"]["name"],model,pars)
             _sep_= ', '
             Dumpfile.write(model + _sep_ + _sep_.join([str(k) for k in pars]) \
                 + _sep_ + str(self.Results[model]) + '\n')
             
-            print(("%s Log(Like) = %s" %(model,self.Results[model])))
+            print("%s Log(Like) = %s" %(model,self.Results[model]))
         
         Inputfile.close()
         Dumpfile.close()
@@ -150,7 +116,7 @@ class ModelTester(Loggin.Message):
           self.Fit.fit(0,covar=True,optimizer=self.config["fitting"]["optimizer"])
           spectrum = self.Fit[self.config['target']['name']].funcs['Spectrum']
           # Get the names of the parameters for the source of interest
-          print(("Loglike Value for ",model,": ",self.Fit.logLike.value()))
+          print("Loglike Value for ",model,": ",self.Fit.logLike.value())
           self.Fit.writeXml(self.folder+"/TestModel/TestModel"+model+".xml")
           loglikevalue = 0
           for comp in self.Fit.components:
@@ -165,11 +131,11 @@ class ModelTester(Loggin.Message):
         SrcSpectrum.getParam('Prefactor').setBounds(1e-7,1e7)
         SrcSpectrum.getParam('Prefactor').setScale(1e-11)
         SrcSpectrum.getParam('Prefactor').setValue(1.)
-        SrcSpectrum.getParam('Prefactor').setFree(1)
+        SrcSpectrum.getParam('Prefactor').setFree(True)
 
         SrcSpectrum.getParam('Index').setBounds(-5,0)
         SrcSpectrum.getParam('Index').setValue(-2)
-        SrcSpectrum.getParam('Index').setFree(1)
+        SrcSpectrum.getParam('Index').setFree(True)
 
         SrcSpectrum.getParam('Scale').setValue(self.config['energy']['emin'])
         SrcSpectrum.getParam('Scale').setBounds(20,3e6)
@@ -184,7 +150,7 @@ class ModelTester(Loggin.Message):
         if pars[1]!=None:
             print("Fixing Index")
             SrcSpectrum.getParam('Index').setScale(pars[1])
-            SrcSpectrum.getParam('Index').setFree(0)
+            SrcSpectrum.getParam('Index').setFree(False)
             par = comp.par_index(name, 'Prefactor')
             comp-freeze(par)
 
@@ -194,36 +160,36 @@ class ModelTester(Loggin.Message):
         SrcSpectrum.getParam('norm').setBounds(1e-7,1e7)
         SrcSpectrum.getParam('norm').setScale(1e-11)
         SrcSpectrum.getParam('norm').setValue(1.)
-        SrcSpectrum.getParam('norm').setFree(1)
+        SrcSpectrum.getParam('norm').setFree(True)
 
         SrcSpectrum.getParam('alpha').setBounds(0,5)
         SrcSpectrum.getParam('alpha').setValue(2)
-        SrcSpectrum.getParam('alpha').setFree(1)
+        SrcSpectrum.getParam('alpha').setFree(True)
 
         SrcSpectrum.getParam('beta').setBounds(0.01,10)
         SrcSpectrum.getParam('beta').setValue(0.5)
-        SrcSpectrum.getParam('beta').setFree(1)
+        SrcSpectrum.getParam('beta').setFree(True)
 
         SrcSpectrum.getParam('Eb').setValue(self.config['energy']['emin'])
-        SrcSpectrum.getParam('Eb').setFree(0)
+        SrcSpectrum.getParam('Eb').setFree(False)
         SrcSpectrum.getParam('Eb').setBounds(20,3e6) 
         
         # Set each non-None parameter to the wanted value and fix it.
         if pars[0]!=None:
             print("Fixing norm")
-            SrcSpectrum.getParam('norm').setFree(0)
+            SrcSpectrum.getParam('norm').setFree(False)
             SrcSpectrum.getParam('norm').setValue(pars[0]/1e-11)
             par = comp.par_index(name, 'norm')
             comp.freeze(par)
         if pars[1]!=None:
             print("Fixing alpha")
-            SrcSpectrum.getParam('alpha').setFree(0)
+            SrcSpectrum.getParam('alpha').setFree(False)
             SrcSpectrum.getParam('alpha').setScale(pars[1])
             par = comp.par_index(name, 'alpha')
             comp.freeze(par)
         if pars[2]!=None:
             print("Fixing beta")
-            SrcSpectrum.getParam('beta').setFree(0)
+            SrcSpectrum.getParam('beta').setFree(False)
             SrcSpectrum.getParam('beta').setScale(pars[2])
             par = comp.par_index(name, 'beta')
             comp.freeze(par)
@@ -235,19 +201,19 @@ class ModelTester(Loggin.Message):
         SrcSpectrum.getParam('Prefactor').setBounds(1e-7,1e7)
         SrcSpectrum.getParam('Prefactor').setScale(1e-11)
         SrcSpectrum.getParam('Prefactor').setValue(1.)
-        SrcSpectrum.getParam('Prefactor').setFree(1)
+        SrcSpectrum.getParam('Prefactor').setFree(True)
 
         SrcSpectrum.getParam('Index1').setBounds(-5,0)
         SrcSpectrum.getParam('Index1').setValue(-2)
-        SrcSpectrum.getParam('Index1').setFree(1)
+        SrcSpectrum.getParam('Index1').setFree(True)
 
         SrcSpectrum.getParam('Index2').setValue(-1)
         SrcSpectrum.getParam('Index2').setBounds(-5,-0.05)
-        SrcSpectrum.getParam('Index2').setFree(0)
+        SrcSpectrum.getParam('Index2').setFree(False)
 
         SrcSpectrum.getParam('Cutoff').setBounds(20,3e6)
         SrcSpectrum.getParam('Cutoff').setValue(1e4)
-        SrcSpectrum.getParam('Cutoff').setFree(1)
+        SrcSpectrum.getParam('Cutoff').setFree(True)
 
         SrcSpectrum.getParam('Scale').setValue(self.config['energy']['emin'])
         SrcSpectrum.getParam('Scale').setBounds(20,3e6)
@@ -255,26 +221,26 @@ class ModelTester(Loggin.Message):
         # Set each non-None parameter to the wanted value and fix it.
         if pars[0]!=None:
             print("Fixing Prefactor")
-            SrcSpectrum.getParam('Prefactor').setFree(0)
+            SrcSpectrum.getParam('Prefactor').setFree(False)
             SrcSpectrum.getParam('Prefactor').setValue(pars[0]/1e-11)
             par = comp.par_index(name, 'Prefactor')
             comp.freeze(par)
         if pars[1]!=None:
             print("Fixing Index1")
             SrcSpectrum.getParam('Index1').setScale(pars[1])
-            SrcSpectrum.getParam('Index1').setFree(0)
+            SrcSpectrum.getParam('Index1').setFree(False)
             par = comp.par_index(name, 'Index1')
             comp.freeze(par)
         if pars[2]!=None:
             print("Fixing Index2")
             SrcSpectrum.getParam('Index2').setScale(pars[2])
-            SrcSpectrum.getParam('Index2').setFree(0)
+            SrcSpectrum.getParam('Index2').setFree(False)
             par = comp.par_index(name, 'Index2')
             comp.freeze(par)
         if pars[3]!=None:
             print("Fixing Cutoff")
             SrcSpectrum.getParam('Cutoff').setScale(pars[3])
-            SrcSpectrum.getParam('Cutoff').setFree(0)
+            SrcSpectrum.getParam('Cutoff').setFree(False)
             par = comp.par_index(name, 'Cutoff')
             comp.freeze(par)
 
